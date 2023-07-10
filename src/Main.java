26b06619ef1f1dce335739d640c8377a567bee43
Leno rafael <73203800+lenors@users.noreply.github.com>
2023-07-10 09:14:32 -0300

    logica = path

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int numero, horas;
+		double valorHora, salario;
+
+		numero = sc.nextInt();
+		horas = sc.nextInt();
+		valorHora = sc.nextDouble();
+
+		salario = valorHora * horas;
+		System.out.println("Numero = " + numero);
+		System.out.printf("Salario = U$ %.2f%n", salario);
+
+		sc.close();
+	}
+}
